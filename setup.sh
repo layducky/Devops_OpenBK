b7d5c5133d7a18bee6c001c55bc116c1783c6707
layducky <tranphuocnhatpnt@gmail.com>
2026-01-04 12:47:21 +0700

    feat: add setup file

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+if ! command -v docker >/dev/null 2>&1; then
+    sudo apt update
+    sudo apt install -y ca-certificates curl gnupg lsb-release
+    curl -fsSL https://get.docker.com | sudo sh
+    sudo usermod -aG docker "$USER"
+    exit 0
+fi
+
+docker compose up -d
